fcf05ec981eaa6506d5c626fa1c6aa93d09a6a15
moraissdev <renanbusinessdev@gmail.com>
2025-04-05 06:29:13 -0300

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//CHAMAR O BOTÃO  
+const button = document.querySelector(".convert-button")
+
+//FUNÇÃO QUE SERÁ EXECUTADA QUANDO HOUVER CLICK NO BOTÃO
+function convertMoney() {
+    //PEGAR SOMENTE O VALOR QUE SERÁ DIGITADO NO INPUT (.value)
+    const input = document.querySelector(".input-value").value
+
+    //DECLARAR O VALOR DO DÓLAR PARA EFETUAR A DIVISÃO PARA CONVERTER
+    const dolar = 5.8
+
+    //VALOR QUE FOI DIGITADO PELO USUÁRIO DIGITADO PELO VALOR ATRIBUÍDO AO DOLAR
+    const convertValue = input / dolar
+
+    //MOSTRAR NA TELA/CONSOLE O VALOR FINAL APÓS A DIVISÃO
+    console.log(convertValue)
+}
+
+//button.addEventListener("click", convertMoney)
