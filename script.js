c8db0cd93cefdb1ba5d409c25373dd2f0a30f965
moraissdev <renanbusinessdev@gmail.com>
2025-04-05 19:12:27 -0300

    commit

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 //CHAMAR O BOTÃO  
 const button = document.querySelector(".convert-button")
 const selectCoin = document.querySelector(".convert-to")
+const coinToConvert = document.querySelector(".convert-from")
+
+function convertSelectCoin() {
+    const nameCoinSelectToConvert = document.querySelector(".coin-title-real")
+    const imgCoinSelectToConvert = document.querySelector(".icon-real")
+
+    if(coinToConvert.value == "euroToConvert") {
+        nameCoinSelectToConvert.innerHTML = "Euro"
+        imgCoinSelectToConvert.src = "assets/euro-icon.png"
+    }
+    if(coinToConvert.value == "dolarToConvert") {
+        nameCoinSelectToConvert.innerHTML = "Dólar Americano"
+        imgCoinSelectToConvert.src = "assets/dolar-icon.png"
+    }
+
+    if(coinToConvert.value == "bitcoinToConvert") {
+        nameCoinSelectToConvert.innerHTML = "Bitcoin"
+        imgCoinSelectToConvert.src = "assets/bitcoin-icon.png"
+    }
+    if(coinToConvert.value == "realToConvert") {
+        nameCoinSelectToConvert.innerHTML = "Real"
+        imgCoinSelectToConvert.src = "assets/real-icon.png"
+    }
+    if(coinToConvert.value == "libraToConvert") {
+        nameCoinSelectToConvert.innerHTML = "Libra Esterlina"
+        imgCoinSelectToConvert.src = "assets/libra-icon.png"
+    }
+}
 
 //FUNÇÃO QUE SERÁ EXECUTADA QUANDO HOUVER CLICK NO BOTÃO
 function convertMoney() {
@@ -95,61 +123,29 @@ function convertCoin() {
         brl.innerHTML = new Intl.NumberFormat("de-de", {
             style: "currency",
             currency: "eur"
-        })
+        }).format((input / libraToConvert).toFixed(2))
     }
     if(coinToConvert.value == "bitcoinToConvert") {
         brl.innerHTML = new Intl.NumberFormat("pt-br", {
             style: "currency",
             currency: "btc"
-        })
+        }).format((input / libraToConvert).toFixed(2))
     }
     if(coinToConvert.value == "realToConvert") {
         brl.innerHTML = new Intl.NumberFormat("pt-br", {
             style: "currency",
             currency: "brl"
-        })
+        }).format((input / libraToConvert).toFixed(2))
         
     }
     if(coinToConvert.value == "libraToConvert") {
         brl.innerHTML = new Intl.NumberFormat("en-gb", {
             style: "currency",
             currency: "gbp"
-        }).format((input) | (input / libraToConvert).toFixed(2))
+        }).format((input / libraToConvert).toFixed(2))
     }
     
 }
-
-const coinToConvert = document.querySelector(".convert-from")
-
-function convertSelectCoin() {
-    const nameCoinSelectToConvert = document.querySelector(".coin-title-real")
-    const imgCoinSelectToConvert = document.querySelector(".icon-real")
-
-    if(coinToConvert.value == "euroToConvert") {
-        nameCoinSelectToConvert.innerHTML = "Euro"
-        imgCoinSelectToConvert.src = "assets/euro-icon.png"
-    }
-    if(coinToConvert.value == "dolarToConvert") {
-        nameCoinSelectToConvert.innerHTML = "Dólar Americano"
-        imgCoinSelectToConvert.src = "assets/dolar-icon.png"
-    }
-
-    if(coinToConvert.value == "bitcoinToConvert") {
-        nameCoinSelectToConvert.innerHTML = "Bitcoin"
-        imgCoinSelectToConvert.src = "assets/bitcoin-icon.png"
-    }
-    if(coinToConvert.value == "realToConvert") {
-        nameCoinSelectToConvert.innerHTML = "Real"
-        imgCoinSelectToConvert.src = "assets/real-icon.png"
-    }
-    if(coinToConvert.value == "libraToConvert") {
-        nameCoinSelectToConvert.innerHTML = "Libra Esterlina"
-        imgCoinSelectToConvert.src = "assets/libra-icon.png"
-    }
-    
-    convertCoin()
-}
-    
 
 button.addEventListener("click", convertCoin)
 button.addEventListener("click", convertMoney)
