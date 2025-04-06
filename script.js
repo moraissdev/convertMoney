805aa1b0d1122b950fb307caeb286605790098cc
moraissdev <renanbusinessdev@gmail.com>
2025-04-06 02:13:11 -0300

    commit

--- script.js
+++ script.js
@@ -5,47 +5,47 @@ const selectCurrencyToConvert = document.querySelector(".convert-to")
 //FUNÇÃO QUE SERÁ EXECUTADA QUANDO HOUVER CLICK NO BOTÃO
 function convertMoney() {
     //CHAMAR O INPUT PARA PEGAR SOMENTE O VALOR QUE SERÁ DIGITADO (.value)
-    const input = document.querySelector(".input-value").value
+    const inputValueEntered = document.querySelector(".input-value").value
     //CHAMAR OS ESPAÇOS QUE OS VALORES FICARÃO ALOCADOS APÓS A CONVERSÃO
     const brl = document.querySelector(".coin-value-real")
-    const anotherCoins = document.querySelector(".coin-value-dolar")
+    const splitValueOutput = document.querySelector(".coin-value-dolar")
 
     //DECLARAR O VALOR DO DÓLAR PARA EFETUAR A DIVISÃO PARA CONVERTER
     const realQuote = 1
     const dollarQuote = 5.8
     const euroQuote = 6.4
     const poundQuote = 7.5
-    const btcQuote = 490000
+    const bitcoinQuote = 490000
 
     if(selectCurrencyToConvert.value == "dolar") { //SE O SELECT ESTIVER SELECIONADO EM DÓLAR, EXECUTA O QUE ESTÁ DENTRO
-        anotherCoins.innerHTML = new Intl.NumberFormat("en-us", {
+        splitValueOutput.innerHTML = new Intl.NumberFormat("en-us", {
             style: "currency",
             currency: "usd"
-        }).format ((input / dollarQuote).toFixed(2))
+        }).format ((inputValueEntered / dollarQuote).toFixed(2))
     }
     if(selectCurrencyToConvert.value == "euro") { //SE O SELECT ESTIVER SELECIONADO EM EURO, EXECUTA O QUE ESTÁ DENTRO
-        anotherCoins.innerHTML = new Intl.NumberFormat("de-de", {
+        splitValueOutput.innerHTML = new Intl.NumberFormat("de-de", {
             style: "currency",
             currency: "eur"  
-        }).format ((input / euroQuote).toFixed(2))
+        }).format ((inputValueEntered / euroQuote).toFixed(2))
     }
     if(selectCurrencyToConvert.value == "libra") { //SE O SELECT ESTIVER SELECIONADO EM LIBRA, EXECUTA O QUE ESTÁ DENTRO
-        anotherCoins.innerHTML = new Intl.NumberFormat("en-gb", {
+        splitValueOutput.innerHTML = new Intl.NumberFormat("en-gb", {
             style: "currency",
             currency: "gbp"
-        }).format((input / poundQuote).toFixed(2))
+        }).format((inputValueEntered / poundQuote).toFixed(2))
     }
     if (selectCurrencyToConvert.value == "bitcoin") {
-        anotherCoins.innerHTML = "₿" + Number(input / btcQuote).toLocaleString("en-US", {
+        splitValueOutput.innerHTML = "₿" + Number(inputValueEntered / bitcoinQuote).toLocaleString("en-US", {
             minimumFractionDigits: 8,
             maximumFractionDigits: 8
         })
     }
     if(selectCurrencyToConvert.value == "real") {
-        anotherCoins.innerHTML = new Intl.NumberFormat("pt-br", {
+        splitValueOutput.innerHTML = new Intl.NumberFormat("pt-br", {
             style: "currency",
             currency: "brl"
-        }).format((input / realQuote).toFixed(2))
+        }).format((inputValueEntered / realQuote).toFixed(2))
     }
 
 
@@ -54,7 +54,7 @@ function convertMoney() {
     brl.innerHTML = new Intl.NumberFormat ("pt-br", {
         style: "currency",
         currency: "brl"
-    }).format(input)
+    }).format(inputValueEntered)
 
 }
 
